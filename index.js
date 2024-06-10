44cae0ac4842c4188e0f7e45632ae41ddfdf858b
BatistIgot <igorbatist089@gmail.com>
2024-06-10 18:32:31 +0300

    restored project

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+let counters = {};
+
+app.post('/increment', (req, res) => {
+    const userId = req.body.userId;
+    console.log(userId);
+
+    if (!userId) {
+        return res.status(400).send('userId is required');
+    }
+
+    if (!counters[userId]) {
+        counters[userId] = 0;
+    }
+
+    counters[userId] += 1;
+
+    console.log(Object.keys(counters));
+    console.log(counters[userId]);
+    res.json({ count: counters[userId] });
+});
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
+});
---
+++ public/js/script.js
@@ -0,0 +1,43 @@
+let userId = "2121";
+let firstName = "Igor";
+let lastName = "Batist";
+
+document.addEventListener('DOMContentLoaded', () => {
+  Telegram.WebApp.ready();
+
+  // Попытка получить данные пользователя из Telegram Web Apps
+  const telegramUserId = Telegram.WebApp.initDataUnsafe.user?.id;
+  const telegramFirstName = Telegram.WebApp.initDataUnsafe.user?.first_name;
+  const telegramLastName = Telegram.WebApp.initDataUnsafe.user?.last_name;
+
+  // Если данные пользователя получены из Telegram, используем их
+  if (telegramUserId) {
+      userId = telegramUserId || userId;
+      firstName = telegramFirstName || firstName; // Используем значение из Telegram, если оно есть
+      lastName = telegramLastName || lastName;   // Используем значение из Telegram, если оно есть
+  }
+
+  // Отображение ID пользователя на странице
+  const userNameElement = document.getElementById('userName');
+  if (firstName) {
+      userNameElement.textContent = firstName + (lastName ? ` ${lastName}` : '');
+  } else {
+      userNameElement.textContent = 'User name not available';
+  }
+});
+
+function incrementCounter() {
+  fetch('/increment', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userId: "2121" }),})
+  .then(response => response.json())
+  .then(data => {
+      document.getElementById("count").innerText = data.count;
+  })
+  .catch(error => {
+      console.error('Ошибка:', error);
+  });
+}
